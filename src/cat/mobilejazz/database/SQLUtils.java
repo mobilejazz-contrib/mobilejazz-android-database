eed6cab9c24d37b04135dd221c7e7d005cbdd240
Hannes Widmoser <widmoser@gmail.com>
2013-02-25 15:18:36 +0100

    created a class DateUtils to handle common date parsing/formatting tasks

--- src/cat/mobilejazz/database/SQLUtils.java
+++ src/cat/mobilejazz/database/SQLUtils.java
@@ -1,44 +1,17 @@
 package cat.mobilejazz.database;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 import android.database.Cursor;
-import android.text.TextUtils;
 import cat.mobilejazz.utilities.debug.Debug;
+import cat.mobilejazz.utilities.io.DateUtils;
 
 public final class SQLUtils {
 
-	private static SimpleDateFormat iso8601FormatUTC = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-	private static SimpleDateFormat sqlFormatUTC = new SimpleDateFormat("yyyy-MM-dd");
-
-	static {
-		iso8601FormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
-		sqlFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
-	}
-
-	public static Date parseTimestamp(String dateString) throws ParseException {
-		if (!TextUtils.isEmpty(dateString)) {
-			return iso8601FormatUTC.parse(dateString);
-		} else {
-			return null;
-		}
-	}
-
-	public static Date parseDate(String dateString) throws ParseException {
-		if (!TextUtils.isEmpty(dateString)) {
-			return sqlFormatUTC.parse(dateString);
-		} else {
-			return null;
-		}
-	}
-
 	public static Date getTimestamp(Cursor cursor, int column) {
 		try {
-			return parseTimestamp(cursor.getString(column));
+			return DateUtils.parseTimestamp(cursor.getString(column));
 		} catch (ParseException e) {
 			Debug.logException(e);
 			return null;
@@ -47,7 +20,7 @@ public final class SQLUtils {
 
 	public static Date getDate(Cursor cursor, int column) {
 		try {
-			return parseDate(cursor.getString(column));
+			return DateUtils.parseDate(cursor.getString(column));
 		} catch (ParseException e) {
 			Debug.logException(e);
 			return null;
@@ -66,24 +39,18 @@ public final class SQLUtils {
 		}
 	}
 
+	public static String formatDate(Date value) {
+		return DateUtils.formatDate(value);
+	}
+
+	public static String formatTimestamp(Date value) {
+		return DateUtils.formatTimestamp(value);
+	}
+
 	public static Long formatBoolean(Boolean value) {
 		if (value == null)
 			return null;
 		return (value) ? 1L : 0L;
-	}
-
-	public static String formatTimestamp(Date date) {
-		if (date != null)
-			return iso8601FormatUTC.format(date);
-		else
-			return null;
-	}
-
-	public static String formatDate(Date date) {
-		if (date != null)
-			return sqlFormatUTC.format(date);
-		else
-			return null;
 	}
 
 	public static String[] getStringArray(Cursor cursor, int columnIndex) {
@@ -130,7 +97,7 @@ public final class SQLUtils {
 		if (arg instanceof Boolean) {
 			return String.valueOf(formatBoolean((Boolean) arg));
 		} else if (arg instanceof Date) {
-			return formatDate((Date) arg);
+			return DateUtils.formatDate((Date) arg);
 		} else {
 			return String.valueOf(arg);
 		}
