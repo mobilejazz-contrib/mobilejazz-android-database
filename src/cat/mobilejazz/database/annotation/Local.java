7834efc5ec11bf2d9d0e2e0c60d96b640a6d18d0
Hannes Widmoser <widmoser@gmail.com>
2013-03-12 16:04:12 +0100

    added documentation for local

--- src/cat/mobilejazz/database/annotation/Local.java
+++ src/cat/mobilejazz/database/annotation/Local.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Denotes a table contract as local. This means that changes to it are never
+ * recorded in the {@link DataProvider} for being later sent to the server. Use
+ * this annotation for tables that are only used to store local data such as for
+ * example a table with caching information ({@code Last-Modified}).
+ * 
+ * @author Hannes Widmoser
+ * 
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Local {
